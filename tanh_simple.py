ae8a0d8b8338eb77243d3436fdce13f32bb5f691
Daniel Grin <dgrin@cr-stokes004.quaker.haverford.edu>
2020-02-26 13:24:23 -0500

    hide the fixed matrix

--- tanh_simple.py
+++ tanh_simple.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 # Create area of x and tanh values (toy model of magnetization)
 x=np.linspace(0,5,100,endpoint=True)
 y=np.tanh(x)
-h=0.01
+h=0.000001
 #Simple forward difference derivative
 def fdif_der(f,x,h):
 #Compute forward difference
@@ -24,23 +24,24 @@ plt.ion()
 
 #plt.figure(1)
 #Set up latex fonts
-plt.rc('text',usetex=True)
-plt.rc('font',**{'family':'serif','serif':['Times New Roman']})
+#plt.rc('text',usetex=True)
+#plt.rc('font',**{'family':'serif','serif':['Times New Roman']})
 
 #Multiple panels to visualize function, derivative, and error
 plt.subplot(211)
 plt.plot(x,y,'b',linewidth=3)
 plt.ylim(0,1.3)
-plt.ylabel('Magnetization'r'$M(T)$',fontsize=18)
+#plt.ylabel('Magnetization'r'$M(T)$',fontsize=18)
 
 #Plot Derivative
 plt.subplot(212)
-plt.plot(x,fdif_der(np.tanh,x,h),linewidth=1)
-plt.plot(x,np.power(np.cosh(x),-2),'r*',markersize=10,linewidth=1)
+plt.plot(x,fdif_der(np.tanh,x,h),color='k',linewidth=4)
+plt.plot(x,np.power(np.cosh(x),-2),'r*',markersize=1,linewidth=4)
 plt.yscale('log')
-plt.ylim([0,10])
-plt.xlabel(r'$1/T~({\rm eV}^{-1})$',fontsize=18)
-plt.ylabel(r'$\frac{dM}{dT}')
+plt.ylim([0.00001,10])
+#plt.xlabel(r'$1/T~({\rm eV}^{-1})$',fontsize=18)
+#plt.ylabel(r'$\frac{dM}{dT}')
+plt.savefig("pleaswork.pdf",format="pdf")
 
 
 
